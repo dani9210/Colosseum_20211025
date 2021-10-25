983d5793fdcb5f4489b708508aa4700f271897a8
김다은 <softeun92@naver.com>
2021-10-25 21:11:10 +0900

    코틀린에서, xml연결하는 binding 변수 세팅

--- app/src/main/java/com/example/colosseum_20211025/MainActivity.kt
+++ app/src/main/java/com/example/colosseum_20211025/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.colosseum_20211025
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.colosseum_20211025.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
     }
+
+
 }
